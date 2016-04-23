e13d1af75fadee760e2ca131b6160d5accd850d4
David Capwell <dcapwell@gmail.com>
2016-04-23 11:21:55 -0700

    renamed predicates

--- src/main/java/com/github/dcapwell/collections/internal/Predicates.java
+++ src/main/java/com/github/dcapwell/collections/internal/Predicates.java
@@ -1,6 +1,6 @@
 package com.github.dcapwell.collections.internal;
 
-public final class Predicate {
+public final class Predicates {
 
     public static <A> A notNull(A value) {
         if (value == null)
